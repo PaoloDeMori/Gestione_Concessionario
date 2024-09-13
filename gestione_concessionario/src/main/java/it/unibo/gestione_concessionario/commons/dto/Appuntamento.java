7205879fafbb3bd826c3e63b279a377d881680f5
PaoloDeMori <150668896+PaoloDeMori@users.noreply.github.com>
2024-09-13 19:31:02 +0200

    Add files via upload

--- gestione_concessionario/src/main/java/it/unibo/gestione_concessionario/commons/dto/Appuntamento.java
+++ gestione_concessionario/src/main/java/it/unibo/gestione_concessionario/commons/dto/Appuntamento.java
@@ -2,12 +2,13 @@ package it.unibo.gestione_concessionario.commons.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Optional;
 public record Appuntamento(
     int idAppuntamento,
     LocalDate data,
     LocalTime ora,
     String tipologia,
-    LocalTime durata,
+    Optional<LocalTime> durata,
     String numero_telaio,
     String  nome_dipendente,
     String  nome_cliente
