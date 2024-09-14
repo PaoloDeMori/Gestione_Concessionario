2ef71296ac6030e7e28a08639167c3f05af70cdd
Luca Latini <l.latini03@gmail.com>
2024-09-14 10:56:03 +0200

    creato metodo creaDipendente

--- gestione_concessionario/src/main/java/it/unibo/gestione_concessionario/commons/dto/Cliente.java
+++ gestione_concessionario/src/main/java/it/unibo/gestione_concessionario/commons/dto/Cliente.java
@@ -1,9 +1,9 @@
 package it.unibo.gestione_concessionario.commons.dto;
 
 public record Cliente(
-    int codCliente,
     String nome,
     String cognome,
     String telefono,
-    String eMail
+    String eMail,
+    String password
 ) {}
