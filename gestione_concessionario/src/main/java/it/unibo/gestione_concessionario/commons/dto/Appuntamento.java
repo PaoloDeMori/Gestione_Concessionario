ab9426e5e55cfb287e4456a575a4eb59e284b1d4
PaoloDeMori <paolo.demori84@gmail.com>
2024-11-05 17:07:45 +0100

    IMPROVED

--- gestione_concessionario/src/main/java/it/unibo/gestione_concessionario/commons/dto/Appuntamento.java
+++ gestione_concessionario/src/main/java/it/unibo/gestione_concessionario/commons/dto/Appuntamento.java
@@ -12,4 +12,10 @@ public record Appuntamento(
     int  id_cliente
 ) {
 
+    @Override
+    public String toString() {
+        return "Giorno " + data.toString() + "Ora " + ora.toString() + "Di tipo: "+ tipologia + "Durata: "+ durata.toString()+ "Per l'auto: " + numero_telaio;
+    }
+    
+
 }
